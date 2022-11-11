34533fdf5967a29898a099bc016bfcf85c466045
SHAILESH141pandit <shaileshpandit141@gmail.com>
2022-11-11 18:32:35 +0530

    11/11/2022

--- meme/main/views.py
+++ meme/main/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
@@ -8,4 +8,29 @@ def home(request):
 
 
 def register(request):
-    return render(request, "Register.html")
+    if request.method == "POST":
+
+        Fname = request.POST["Fname"]
+        Lname = request.POST["Lname"]
+        tel = request.POST["tel"]
+        email = request.POST["Email"]
+        password = request.POST["Password"]
+
+        print(f"    First name : {Fname}")
+        print(f"     Last name : {Lname}")
+        print(f"Contect number : {tel}")
+        print(f"         Email : {email}")
+        print(f"      Password : {password}")
+        
+        print("----------------------------")
+        print(request.POST)
+        print("----------------------------")
+        var = request.POST
+        for i in var:
+            print(i,":",var[i])
+        print("----------------------------")
+
+
+        return HttpResponse("<h1>This is GET requist.")
+    else :
+        return render(request, "Register.html")
